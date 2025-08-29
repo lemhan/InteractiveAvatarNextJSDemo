113899318a365a307da3c2b5d60743e2cc36200f
lemhan <leandrohanada@gmail.com>
2025-08-29 16:45:26 -0300

    Update index.tsx

--- components/AvatarConfig/index.tsx
+++ components/AvatarConfig/index.tsx
@@ -54,6 +54,86 @@ export const AvatarConfig: React.FC<AvatarConfigProps> = ({
 
   return (
     <div className="relative flex flex-col gap-4 w-[550px] py-8 max-h-full overflow-y-auto px-4">
+      <Field label="Custom Knowledge Base ID">
+        <Input
+          value={config.knowledgeId}
+          />
+      </Field>
+      <Field label="Avatar ID">
+        <Input
+          value={config.avatar_id}
+          />
+      </Field>
+      <Field label="Language">
+        <Input
+          value={config.language}
+          />
+      </Field>
+      <Field label="Avatar Quality">
+        <Input
+          value={config.quality}
+          />
+      </Field>
+      <Field label="Voice Chat Transport">
+        <Input
+          value={config.voiceChatTransport}
+          />
+      </Field>
+      {showMore && (
+        <>
+          <h1 className="text-zinc-100 w-full text-center mt-5">
+            Voice Settings
+          </h1>
+          <Field label="Custom Voice ID">
+            <Input
+              placeholder="Enter custom voice ID"
+              value={config.voice?.voiceId}
+              onChange={(value) =>
+                onChange("voice", { ...config.voice, voiceId: value })
+              }
+            />
+          </Field>
+          <Field label="Emotion">
+            <Select
+              isSelected={(option) => option === config.voice?.emotion}
+              options={Object.values(VoiceEmotion)}
+              renderOption={(option) => option}
+              value={config.voice?.emotion}
+              onSelect={(option) =>
+                onChange("voice", { ...config.voice, emotion: option })
+              }
+            />
+          </Field>
+          <Field label="ElevenLabs Model">
+            <Select
+              isSelected={(option) => option === config.voice?.model}
+              options={Object.values(ElevenLabsModel)}
+              renderOption={(option) => option}
+              value={config.voice?.model}
+              onSelect={(option) =>
+                onChange("voice", { ...config.voice, model: option })
+              }
+            />
+          </Field>
+          <h1 className="text-zinc-100 w-full text-center mt-5">
+            STT Settings
+          </h1>
+          <Field label="Provider">
+            <Select
+              isSelected={(option) => option === config.sttSettings?.provider}
+              options={Object.values(STTProvider)}
+              renderOption={(option) => option}
+              value={config.sttSettings?.provider}
+              onSelect={(option) =>
+                onChange("sttSettings", {
+                  ...config.sttSettings,
+                  provider: option,
+                })
+              }
+            />
+          </Field>
+        </>
+      )}
     </div>
   );
 };
